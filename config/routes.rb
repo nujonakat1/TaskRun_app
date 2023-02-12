74d0ca8996ce8719d6d3f3b149c2ffa74506252d
nujonakat1 <takanojun@icloud.com>
2023-02-12 10:21:21 +0900

    modelとcontrollerの生成完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "tasks#index"
+  resources :tasks
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_11_162227) do
+  create_table "tasks", charset: "utf8mb3", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
