4154047986af8b50d237050467fbbcbec98d5e7a
nujonakat1 <takanojun@icloud.com>
2023-02-13 15:21:58 +0900

    index,new,show,editアクション実装

--- Gemfile
+++ Gemfile
@@ -54,6 +54,13 @@ gem "sassc-rails"
 gem "bootstrap"
 gem "bootstrap-sass"
 
+# Slimの導入
+gem "slim-rails"
+gem "html2slim"
+
+# エラーの日本語表示用gem
+gem 'rails-i18n'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module TaskrunApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -18,5 +20,8 @@ module TaskrunApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :ja
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
   end
 end
