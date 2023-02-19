635ebaa3ad628bf8f752b253664dac671943a150
nujonakat1 <takanojun@icloud.com>
2023-02-19 23:44:22 +0900

    JavaScriptとCSSの圧縮方法を指定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,9 @@ Rails.application.configure do
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
+  #  JavaScriptとCSSの圧縮方法を指定
+  config.assets.js_compressor = uglifier
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
