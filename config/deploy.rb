ff3b0b01fe2cf3f9ad21a8c934a289cc8a14a0b5
nujonakat1 <takanojun@icloud.com>
2023-02-28 23:25:21 +0900

    自動デプロイのError対応

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,9 @@ set :deploy_to, '/var/www/TaskRun_app'
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:nujonakat1/TaskRun_app.git'
 
+# ブランチを指定する
+set :branch, "main"
+
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
